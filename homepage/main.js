113a89c7ec3159b0eb40951206ab94ba758686a1
Paweł Słowik <ffpawel.ps2002@gmail.com>
2022-02-19 21:26:12 +0100

    Add hamburger menu

--- homepage/main.js
+++ homepage/main.js
@@ -9,3 +9,23 @@ const introduction = function(name, age){
 }
 
 introduction("Paweł" , 20);
+
+const headerButton = document.querySelector(".header__button--js");
+
+const clickButton = function(){
+    console.log("Kliknałeś");
+    const title = document.querySelector(".header__title");
+    title.innerHTML = "Pozdrawiam";
+    
+}
+
+headerButton.addEventListener("click" , clickButton);
+
+const navButton = document.querySelector(".navigation__button--js");
+
+
+const clickNavButton = function(){
+    const navContainer = document.querySelector(".navigation__container")
+    navContainer.classList.toggle("navigation__container--js")
+}
+navButton.addEventListener("click" , clickNavButton);
