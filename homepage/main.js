b0a8eaed6e70b38cf54bcac744b8c666da727f16
Paweł Słowik <ffpawel.ps2002@gmail.com>
2022-02-19 19:17:10 +0100

    Add media queries

--- homepage/main.js
+++ homepage/main.js
@@ -3,3 +3,9 @@ console.log("hej");
 var takeClass = document.querySelector(".article__text--js");
 
 takeClass.innerHTML = "Da się :)";
+
+const introduction = function(name, age){
+    console.log("Cześć mam na imię " + name  + " i mam " + age +"lat.");
+}
+
+introduction("Paweł" , 20);
